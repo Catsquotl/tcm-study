09d59d2f221d46bd9178e6fc18722190e684a91d
Catsquotl <Catsquotl@gmail.com>
2011-03-04 22:04:19 +0100

    cleaned up a bit. added listbox for ZangFu choice..

--- zangfu.rb
+++ zangfu.rb
@@ -1,11 +1,15 @@
 
 class ZangFu
 	require './acupunt'
- 	attr_reader :punten ,:functies
+ 	attr_reader :punten ,:functies, :points
+ 	def initialize points
+ 	@points = points
+ 	self.set_punten
+ 	end
 	
-		def set_punten(filename)
+		def set_punten
 			@punten=[]
-			File.open("#{filename}", "r").each {|e|
+			File.open("#{@points}", "r").each {|e|
 				e.chomp!
 				b=e.split(";")
 				@punten.push(Acupunt.new(b[0],b[1],b[2],b[3],b[4],b[5]))
